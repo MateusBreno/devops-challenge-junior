dd5bac370a8cfa74ea0d6199823f2807e0c8dbce
MateusBreno <brenoconstantino80@gmail.com>
2025-03-26 22:47:08 -0300

    Correção dos erros do DevOps Challenge

--- devops_challenge.php
+++ devops_challenge.php
@@ -1,86 +1,95 @@
+<?php
 /**
  * @package Devops_challenge_Junior
  * @version 1.0
  */
 /*
-Plugin Name: Devops challenge Júnior
+Plugin Name: Devops Challenge Júnior
 Plugin URI: https://apiki.com/
 Description: Sabe de nada, inocente! Ordinária!!
 Author: Apiki WordPress
 Version: 1.0
 */
 
-$global_lyrics;
+// 1. Inicialização da variável global
+$global_lyrics = '';
 
 function apiki_segura_o_tchan() {
-	$lyrics = $global_lyrics;
-	
-	$lyrics = "Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	Olha, domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	O pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Segure o tchan
-	Amare o tchan
-	Segure o tchan tchan tchan tchan
-	Depois de nove meses você vê o resultado
-	Esse é o Gera Samba arrebentando no pedaço
-	Joga ela no meio, mete em cima, mete embaixo";
+    // 2. Uso correto da variável global
+    global $global_lyrics;
+    
+    $global_lyrics = "Pau que nasce torto nunca se endireita
+    Menina que requebra a mãe pega na cabeça
+    Pau que nasce torto nunca se endireita
+    Menina que requebra a mãe pega na cabeça
+    Domingo ela não vai (vai, vai)
+    Domingo ela não vai não (vai, vai, vai)
+    Olha, domingo ela não vai (vai, vai)
+    Domingo ela não vai não (vai, vai, vai)
+    O pau que nasce torto nunca se endireita
+    Menina que requebra a mãe pega na cabeça
+    Pau que nasce torto nunca se endireita
+    Menina que requebra a mãe pega na cabeça
+    Segure o tchan
+    Amarre o tchan
+    Segure o tchan tchan tchan tchan
+    Depois de nove meses você vê o resultado
+    Esse é o Gera Samba arrebentando no pedaço
+    Joga ela no meio, mete em cima, mete embaixo";
 
-	$lyrics = explode( "\n", $lyrics )
+    // 4. Correção da falta de ponto e vírgula
+    $lyrics = explode("\n", $global_lyrics);
 
-	return wptexturize( $lyrics[ mt_rand( count( $lyrics ) - 1, 0 ) ] );
+    // 5. Correção na função mt_rand (invertendo parâmetros)
+    return wptexturize($lyrics[mt_rand(0, count($lyrics) - 1)]);
 }
 
 function devops_challenge() {
-	$chosen = $undefined_variable;
-	$lang   = '';
-	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
-		$lang = ' lang="en"';
-	}
+    // 7. Correção de variável indefinida
+    $chosen = apiki_segura_o_tchan();
 
-	printf(
-		'O texto é: %s %s %s',
-		__( 'Segure o Tchan, by Apiki WordPress:' ),
-		$lang
-	);
+    $lang = '';
+    if ('en_' !== substr(get_user_locale(), 0, 3)) {
+        $lang = ' lang="en"';
+    }
+
+    // 8. Ajuste da função printf para argumentos corretos
+    printf(
+        '<p %s>%s %s</p>',
+        esc_attr($lang),
+        esc_html__('Segure o Tchan, by Apiki WordPress:', 'devops_challenge_junior'),
+        esc_html($chosen)
+    );
 }
 
-add_action( '', 'devops_challenge' );
+// 6. Correção no hook com identificador correto
+add_action('admin_notices', 'devops_challenge');
 
 function devop_css() {
-	echo "
-	<style type='text/css'>
-	#devop {
-		float: right;
-		padding: 5px 10px;
-		margin: 0;
-		font-size: 12px;
-		line-height: 1.6666;
-	}
-	.rtl #devop {
-		float: left;
-	}
-	.block-editor-page #devop {
-		display: none;
-	}
-	@media screen and (max-width: 782px) {
-		#devop,
-		.rtl #devop {
-			float: none;
-			padding-left: 0;
-			padding-right: 0;
-		}
-	}
-	</style>
-	";
+    // 10. Hook de CSS correto
+    echo "<style>
+    #devop {
+        float: right;
+        padding: 5px 10px;
+        margin: 0;
+        font-size: 12px;
+        line-height: 1.6666;
+    }
+    .rtl #devop {
+        float: left;
+    }
+    .block-editor-page #devop {
+        display: none;
+    }
+    @media screen and (max-width: 782px) {
+        #devop, .rtl #devop {
+            float: none;
+            padding-left: 0;
+            padding-right: 0;
+        }
+    }
+    </style>";
 }
 
-add_action( 'admin_head', 'devop_css' );
+// 9. Aplicação do CSS diretamente ao painel
+add_action('admin_head', 'devop_css');
